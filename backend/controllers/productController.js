d775f16b57d6941a95e2c76b48ada6c09e7b0d03
Abhra pal <palabhra1612@gmail.com>
2023-10-17 00:06:25 +0530

    Adding Search, Filter & Pagination to backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 //Create Product --Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -10,10 +11,16 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-//Get All Products --Admin
+//Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json({ success: true, products });
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+  res.status(200).json({ success: true, products, productCount });
 });
 
 // Get Product Details
@@ -21,7 +28,7 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
+    return next(new ErrorHandler("Product is not found", 404));
   }
 
   res.status(200).json({
